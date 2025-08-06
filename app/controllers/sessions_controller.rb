4c9f190a94c664c8334d2c88ba808a74aec0ce69
Tosin <tadewumi2@rrc.ca>
2025-08-06 09:26:32 -0500

    admins to be redirected to the main page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,13 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:notice] = "Welcome back, #{user.name}!"
-
-      # Redirect admin users to admin dashboard
-      if user.admin?
-        redirect_to admin_root_path
-      else
-        redirect_to root_path
-      end
+      redirect_to root_path
     else
       flash.now[:alert] = "Invalid email or password"
       render :new
